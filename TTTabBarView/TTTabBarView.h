cb5200de5f53fc70578e8936ab011db0395340df
mac <491365585@qq.com>
2019-01-15 15:19:31 +0800

    Initial Commit

---
+++ TTTabBarView/TTTabBarView.h
@@ -0,0 +1,19 @@
+//
+//  TTTabBarView.h
+//  TTTabBarView
+//
+//  Created by mac on 2019/1/15.
+//  Copyright © 2019年 tai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TTTabBarView.
+FOUNDATION_EXPORT double TTTabBarViewVersionNumber;
+
+//! Project version string for TTTabBarView.
+FOUNDATION_EXPORT const unsigned char TTTabBarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TTTabBarView/PublicHeader.h>
+
+
